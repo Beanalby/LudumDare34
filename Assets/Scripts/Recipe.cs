a649b74128d0ceb97bb3ac0dc2780631601031f6
U-Beanolith\ninji <Jason Viers>
2015-12-13 20:42:15 -0500

    Added models.  So may models.

--- Assets/Scripts/Recipe.cs
+++ Assets/Scripts/Recipe.cs
@@ -6,7 +6,8 @@ namespace LudumDare34 {
     public class Recipe : MonoBehaviour {
 
         public string displayName;
+        public GameObject modelTopPrefab;
+        public GameObject modelBottomPrefab;
         public Ingredient[] ingredients;
-
     }
 }
